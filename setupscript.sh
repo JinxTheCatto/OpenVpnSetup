ed96830035e451d0cc16a363bef255f3d9c97ba5
JinxTheCat <59938808+JinxTheCatto@users.noreply.github.com>
2023-09-20 18:12:36 -0700

    s

--- setupscript.sh
+++ setupscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'start'
-# curl -O https://raw.githubusercontent.com/JinxTheCatto/OpenVpnSetup/main/setupscript.sh chmod +x setupscript.sh
-
-#./setupscript.sh
+apt update
+# curl -O https://raw.githubusercontent.com/JinxTheCatto/OpenVpnSetup/main/setupscript.sh
+# chmod +x setupscript.sh && ./setupscript.sh

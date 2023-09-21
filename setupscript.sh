d307904ee655ea9b6e51539331b3b86cd8797d60
JinxTheCat <59938808+JinxTheCatto@users.noreply.github.com>
2023-09-20 18:43:24 -0700

    s

--- setupscript.sh
+++ setupscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo 'START'
-sudo apt update && sudo apt upgrade
-sudo apy install openvpn
-curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
-chmod +x openvpn-install.sh
-./openvpn-install.sh
-echo 'END'
 # curl -O https://raw.githubusercontent.com/JinxTheCatto/OpenVpnSetup/main/setupscript.sh
 # chmod +x setupscript.sh && ./setupscript.sh
+echo 'SCRIPT START'
+apt update && apt upgrade
+apt install openvpn
+curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
+chmod +x openvpn-install.sh && ./openvpn-install.sh
+systemctl restart openvpn
+echo 'SCRIPT END'
